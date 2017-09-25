b1f4875610d177b85af9aaabd5d15a3a28083a97
sgsen <sohamgsen@gmail.com>
2017-09-25 11:59:43 +0530

    Adding placeholder for trustscore and helper function

---
+++ refreshdailylimits/trustscore.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+#what factors to include here? 
+#reattempts returns
+#how quickly bounces are repaid
+#how many bounces of cheques
+#how many bounces of credit
+
---
+++ refreshdailylimits/writedatatogsheets.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+
+#Puja can you try to make a live version of this?
+#https://www.dataiku.com/learn/guide/code/python/export-a-dataset-to-google-spreadsheets.html
